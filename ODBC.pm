2227db98c689becfb4eae681e6f5938ba0327b7a
Jason A. Crome <cromedome@cpan.org>
2004-10-27 14:28:06 +0000

    Import of CROMEDOME/CGI-Session-ODBC-0.5 from CPAN.
    gitpan-cpan-distribution: CGI-Session-ODBC
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         CROMEDOME/CGI-Session-ODBC-0.5.tar.gz
    gitpan-cpan-author:       CROMEDOME
    gitpan-cpan-maturity:     released

--- ODBC.pm
+++ ODBC.pm
@@ -1,32 +1,69 @@
-# CGI::Session::ODBC - ODBC driver for CGI::Session.
-#
-# Copyright (C) 2003 by Jason A. Crome, cromedome@cpan.org.
-#
-# This module is directly based on CGI::Session::PostgreSQL by Cosimo
-# Streppone, and indirectly based on CGI::Session::MySQL module
-# by Sherzod Ruzmetov.
-#
-# Revision History:
-# - 2003/08/08 by Jason A. Crome: Initial version.
-#
-
 package CGI::Session::ODBC;
 
+=pod
+
+=head1 NAME
+
+CGI::Session::ODBC - ODBC driver for CGI::Session
+
+=head1 VERSION
+
+    Version 0.50
+
+=head1 SYNOPSIS
+
+    use CGI::Session;
+    $session = new CGI::Session("driver:ODBC", undef, { Handle => $dbh });
+
+For more examples, consult the L<CGI::Session> manual.
+
+=head1 DESCRIPTION
+
+CGI::Session::ODBC is a CGI::Session driver to store session data in any
+ODBC-capable database.  To write your own drivers for C<CGI::Session>,
+please refer to the L<CGI::Session> manual.
+
+=head1 USAGE
+
+To store session data in the ODBC-compliant database, you must first
+create a suitable table for your session data to live.  In many instances,
+the following SQL will accomplish this:
+
+    CREATE TABLE sessions (
+        id CHAR(32) NOT NULL,
+        a_session TEXT NOT NULL
+    );
+
+You can add any number of additional columns to the table, but the columns
+"id" and "a_session" are required as defined above.  If you want to store the
+session data in a table other than "sessions", you will also need to specify
+the B<TableName> attribute as an argument:
+
+    use CGI::Session;
+
+    $session = new CGI::Session("driver:ODBC", undef,
+        { Handle => $dbh, TableName => 'my_sessions' });
+
+There are no special provisions made for row-level locking.  ODBC is intended
+to be platform independent, and to the best of the author's knowledge at the
+time of this writing, there is no platform independent manner of implementing
+row locking.  Please contact the author with your experiences with this module
+in that regard.
+
+=cut
+
 use strict;
-# Inheriting necessary functionalities from the
-# following libraries. Do not change it unless
-# you know what you are doing!
 use base qw(
     CGI::Session
     CGI::Session::ID::MD5
     CGI::Session::Serialize::Default
 );
 
-# Driver specific libraries
+# Driver specific properties
 use vars qw($VERSION $TABLE_NAME);
 
-$VERSION = '0.1';
-$TABLE_NAME = 'sessions';
+$VERSION = '0.5';
+$TABLE_NAME ||= 'sessions';
 
 ##
 ## Driver methods follow
@@ -37,49 +74,40 @@ sub store
 {
     my ($self, $sid, $options, $data) = @_;
     my $dbh = $self->ODBC_dbh($options);
-    my $db_data;
+    my $id = -1;
+    my $sth;
 
     eval
     {
-    	# Does this session already exist?
-        $db_data = $dbh->selectrow_array(
-            "SELECT a_session   " .
-            "  FROM $TABLE_NAME " .
-            " WHERE id =        " . $dbh->quote($sid)
-        );
+        $id = $dbh->selectrow_array(qq|SELECT a_session FROM $TABLE_NAME WHERE id = ?|, undef, $sid);
     };
-
     if($@)
     {
-        $self->error("Couldn't fetch session data for '$sid'");
+        $self->error("Couldn't fetch session data for '$sid': $@");
         return undef;
     }
 
+    my $freeze_data = $self->freeze($data);
+    
     eval
     {
-        if($db_data)
+        if(defined $id)
         {
-        	# Update the existing session with new information
-            $dbh->do(
-                "UPDATE $TABLE_NAME " .
-                "SET a_session=     " . $dbh->quote($self->freeze($data)) .
-                "WHERE id =         " . $dbh->quote($sid)
-            );
+            $sth = $dbh->prepare(qq|UPDATE $TABLE_NAME SET a_session = ? WHERE id = ?|);
         }
         else
         {
-        	# This is a new session
-            $dbh->do(
-                "INSERT INTO $TABLE_NAME (id, a_session) ".
-                "VALUES (" . $dbh->quote($sid) . ", " . $dbh->quote($self->freeze($data)) . ") "
-            );
+            $sth = $dbh->prepare(qq|INSERT INTO $TABLE_NAME (a_session, id) VALUES (?,?)|);
         }
+
+        $sth->bind_param(1, $freeze_data, DBI::SQL_LONGVARCHAR);
+        $sth->execute($freeze_data, $sid);
     };
 
-    if($@)
+	if($@)
     {
         $self->error("Error updating session '$sid': $@");
-        warn("Error updating session '$sid': $@");
+        warn "Error updating session '$sid': $@";
         return undef;
     }
 
@@ -93,19 +121,17 @@ sub retrieve
 
     # Fetch our session data.
     my $dbh = $self->ODBC_dbh($options);
-	my $data;
+    my $data;
 
-    eval {
-    	$data = $dbh->selectrow_array(
-    		"SELECT a_session   " .
-            "  FROM $TABLE_NAME " .
-			" WHERE id =        " . $dbh->quote($sid)
-	    );
-	};
-
-	if($@)
+    eval
     {
-        $self->error("Couldn't fetch session data for '$sid'");
+        $data = $dbh->selectrow_array(qq|SELECT a_session FROM $TABLE_NAME WHERE id = ?|, undef, $sid);
+    };
+
+    if($@)
+    {
+        $self->error("Couldn't fetch session data for '$sid': $@");
+        warn "Couldn't fetch session data for '$sid': $@";
         return undef;
     }
 
@@ -118,32 +144,13 @@ sub remove
 {
     my ($self, $sid, $options) = @_;
     my $dbh = $self->ODBC_dbh($options);
-    my $data;
 
-    eval
-    {
-    	# Does this session exist?
-        $data = $dbh->selectrow_array(
-            "SELECT a_session   " .
-            "  FROM $TABLE_NAME " .
-            " WHERE id =        " . $dbh->quote($sid)
-        );
-    };
-
-    if($@) 
-    {
-        $self->error("Couldn't fetch session data for '$sid'");
-        return undef;
-    }
-
-    eval 
-    {
-        $dbh->do("DELETE FROM $TABLE_NAME WHERE id = " . $dbh->quote($sid));
-    };
+    eval { $dbh->do(qq|DELETE FROM $TABLE_NAME WHERE id = ?|, undef, $sid); };
 
     if($@)
     {
-        $self->error("Couldn't delete session '$sid'");
+        $self->error("Couldn't delete session '$sid': $@");
+        warn "Couldn't delete session '$sid': $@";
         return undef;
     }
 
@@ -158,7 +165,7 @@ sub teardown
 
     # Call commit() if we're not set to auto-commit
     $dbh->commit() unless $dbh->{AutoCommit};
-
+    
     # Disconnect
     $dbh->disconnect() if $self->{ODBC_disconnect};
 
@@ -166,11 +173,11 @@ sub teardown
 }
 
 # Create the driver
-sub ODBC_dbh 
+sub ODBC_dbh
 {
     my ($self, $options) = @_;
     my $args = $options->[1] || {};
-    
+
     return $self->{ODBC_dbh} if defined $self->{ODBC_dbh};
     $TABLE_NAME = $args->{TableName} if defined $args->{TableName};
 
@@ -190,68 +197,6 @@ sub ODBC_dbh
     return $self->{ODBC_dbh};
 }
 
-1;
-
-=pod
-
-=head1 NAME
-
-CGI::Session::ODBC - ODBC driver for CGI::Session
-
-=head1 SYNOPSIS
-
-    use CGI::Session;
-    $session = new CGI::Session("driver:ODBC", undef, { Handle => $dbh });
-
-For more examples, consult the L<CGI::Session> manual.
-
-=head1 DESCRIPTION
-
-CGI::Session::ODBC is a CGI::Session driver to store session data in any
-ODBC-capable database.  To write your own drivers for B<CGI::Session>, 
-please refer to the L<CGI::Session> manual.
-
-=head1 STORAGE
-
-To store session data in the ODBC-compliant database, you must first
-create a suitable table for your session data to live.  In many instances,
-the following SQL will accomplish this:
-
-    CREATE TABLE sessions (
-        id CHAR(32) NOT NULL,
-        a_session TEXT NOT NULL
-    );
-
-You can add any number of additional columns to the table, but the columns
-"id" and "a_session" are required as defined above.  If you want to store the 
-session data in a table other than "sessions", you will also need to specify 
-the B<TableName> attribute as an argument:
-
-    use CGI::Session;
-
-    $session = new CGI::Session("driver:ODBC", undef,
-        { Handle => $dbh, TableName => 'my_sessions' });
-
-There are no special provisions made for row-level locking.  ODBC is intended
-to be platform independent, and to the best of the author's knowledge at the
-time of this writing, there is no platform independent manner of implementing
-row locking.  Please contact the author with your experiences with this module
-in that regard.
-
-=head1 COPYRIGHT
-
-Copyright (C) 2003 by Jason A. Crome. All rights reserved.
-
-This library is free software and can be modified and distributed under the 
-same terms as Perl itself.
-
-=head1 AUTHOR
-
-Jason A. Crome <cromedome@cpan.org>, heavily based on the 
-CGI::Session::PostgreSQL by Cosimo Streppone, which was heavily based on the 
-CGI::Session::MySQL driver by Sherzod Ruzmetov, original author of 
-CGI::Session.
-
 =head1 SEE ALSO
 
 =over 4
@@ -270,7 +215,7 @@ L<CGI::Session::CookBook|CGI::Session::CookBook> - practical solutions for real 
 
 =item *
 
-B<RFC 2965> - "HTTP State Management Mechanism" found at ftp://ftp.isi.edu/in-notes/rfc2965.txt
+C<RFC 2965> - "HTTP State Management Mechanism" found at ftp://ftp.isi.edu/in-notes/rfc2965.txt
 
 =item *
 
@@ -282,4 +227,25 @@ L<Apache::Session|Apache::Session> - another fine alternative to CGI::Session
 
 =back
 
+=head1 COPYRIGHT
+
+Copyright (C) 2003-2004 by Jason A. Crome. All rights reserved.
+
+This library is free software and can be modified and distributed under the
+same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jason A. Crome C<< <cromedome@cpan.org> >>.
+
+=head1 CREDITS
+
+This module is directly based on CGI::Session::PostgreSQL by Cosimo Streppone,
+and indirectly based on CGI::Session::MySQL module by Sherzod Ruzmetov.
+
+Many thanks to Rhesa Rozendaal for the placeholder patch.
+
 =cut
+
+1; # End of CGI::Session::ODBC
+
